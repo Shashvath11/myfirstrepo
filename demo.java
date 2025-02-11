b39f29217c4c87be27402f380ec45c1eb5ee3d40
Shashvath11 <shashvathmas@gmail.com>
2025-02-11 15:57:35 +0530

    feat : added a addition program

---
+++ demo.java
@@ -0,0 +1,9 @@
+public class demo{
+    public static void main(String[] args) {
+        int a = 10;
+        int b= 20;
+        System.out.println("Addition of 2 numbers: "+ (a+b));
+        System.out.println("Hello Shashvath..!");
+    }
+
+}
